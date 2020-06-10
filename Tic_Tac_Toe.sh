b063d1236b9e9c929cb37b5885bc84a592a99230
Srijan-18 <srijan.mishrasir@gmail.com>
2020-06-11 00:07:14 +0530

    Decision of Sign for User and Computer respectively

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -1,12 +1,14 @@
 #CONSTANTS
 
-TOSS_USER="1"
-
+TOSS_USER=1
+COMPUTER_X=1
 #VARIABLES
 
 declare -a gameBoard
 cell=0
-toss="none"
+toss=2
+computerSign=""
+userSign=""
 
 #FUNCTION TO RESET GAME BOARD WITH RESPECTIVE CELL NUMBERS
 
@@ -26,15 +28,52 @@ toss()
 	toss=$((RANDOM%2))
 	if [ $toss -eq $TOSS_USER ]
 	then
-		toss="USER"
 		echo "			You Won the Toss to make first move"
 	else
-		toss="COMPUTER"
 		echo "			Computer Won the Toss to make first move"
 	fi
+}
+
+#FUNCTION TO DECIDE SIGN OF USER AND COMPUTER
+
+signDecision()
+{
+	if [ $toss -eq $TOSS_USER ]
+	then
+		while [[ "$userSign" == "" ]]
+		do
+			echo""
+			read -p "			Enter your choice of 'X' or 'O' (both are alphabets): " userSign
+			if [[ "$userSign" == "O" || "$userSign" == "o" ]]
+			then
+				userSign="O"
+				computerSign="X"
+			elif [[ "$userSign" == "X" || "$userSign" == "x" ]]
+			then
+				userSign="X"
+				computerSign="O"
+			else
+				echo""
+				echo "			INVALID INPUT PLEASE ENTER EITHER OF TWO OPTIONS"
+				userSign=""
+			fi
+		done
+	else
+		computerSign=$((RANDOM%2))
+		if (( computerSign=COMPUTER_X ))
+		then
+			computerSign="X"
+			userSign="O"
+		else
+			computerSign="O"
+			userSign="X"
+		fi
+	fi
+echo "			USER : $userSign"
+echo "			COMPUTER : $computerSign"
 }
 
 printf "\n		***** WELCOME TO TIC-TAC-TOE GAME SIMULATOR ***** \n\n\n "
 resetGameBoard
 toss
-
+signDecision
