fc1f8c0c31da27611894fe9fe596dec82bcabf70
Srijan-18 <srijan.mishrasir@gmail.com>
2020-06-10 22:56:33 +0530

    Reset of locations on board to respective cell numbers

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -1,2 +1,19 @@
+#VARIABLES
+
+
+declare -a gameBoard
+cell=0
+
+#FUNCTION TO RESET GAME BOARD WITH RESPECTIVE CELL NUMBERS
+
+resetGameBoard()
+{
+	for((cell=1;cell<=9;cell++))
+	do
+		gameBoard[cell]=$cell
+	done
+}
+
 printf "\n		****** WELCOME TO TIC-TAC-TOE GAME SIMULATOR ***** \n\n\n "
+resetGameBoard
 
