00ed70837f4c985238b771e15b764bd1f72b310c
Srijan-18 <srijan.mishrasir@gmail.com>
2020-06-10 23:24:02 +0530

    Toss to decide who will make first move between user and computer

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -1,8 +1,12 @@
-#VARIABLES
+#CONSTANTS
 
+TOSS_USER="1"
+
+#VARIABLES
 
 declare -a gameBoard
 cell=0
+toss="none"
 
 #FUNCTION TO RESET GAME BOARD WITH RESPECTIVE CELL NUMBERS
 
@@ -14,6 +18,23 @@ resetGameBoard()
 	done
 }
 
-printf "\n		****** WELCOME TO TIC-TAC-TOE GAME SIMULATOR ***** \n\n\n "
+#FUNCTION TO TOSS AND DECIDE WHO STARTS THE GAME
+
+toss()
+{
+
+	toss=$((RANDOM%2))
+	if [ $toss -eq $TOSS_USER ]
+	then
+		toss="USER"
+		echo "			You Won the Toss to make first move"
+	else
+		toss="COMPUTER"
+		echo "			Computer Won the Toss to make first move"
+	fi
+}
+
+printf "\n		***** WELCOME TO TIC-TAC-TOE GAME SIMULATOR ***** \n\n\n "
 resetGameBoard
+toss
 
