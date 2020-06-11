5515d33d4dae2b27e89b9b9588adfa3c739ca1d2
Srijan-18 <srijan.mishrasir@gmail.com>
2020-06-11 05:54:45 +0530

    Board display and User input to make a move in a particular cell

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -1,3 +1,4 @@
+
 #CONSTANTS
 
 TOSS_USER=1
@@ -9,7 +10,8 @@ cell=0
 toss=2
 computerSign=""
 userSign=""
-
+userChoice=0
+cellOccupied=0
 #FUNCTION TO RESET GAME BOARD WITH RESPECTIVE CELL NUMBERS
 
 resetGameBoard()
@@ -69,11 +71,64 @@ signDecision()
 			userSign="X"
 		fi
 	fi
+
+echo "			Signs are :"
 echo "			USER : $userSign"
 echo "			COMPUTER : $computerSign"
 }
 
+#FUNCTION TO DISPLAY BOARD
+
+display()
+{
+	echo " "
+	echo "			     ****CURRENT BOARD STATUS****"
+	echo " "
+	echo "				||  ${gameBoard[1]}  ||  ${gameBoard[2]}  ||  ${gameBoard[3]}  ||"
+	echo "				-----------------------"
+	echo "				||  ${gameBoard[4]}  ||  ${gameBoard[5]}  ||  ${gameBoard[6]}  ||"
+	echo "				-----------------------"
+	echo "				||  ${gameBoard[7]}  ||  ${gameBoard[8]}  ||  ${gameBoard[9]}  ||"
+	echo " "
+}
+
+#FUNCTION TO TAKE INPUT FROM USER
+
+userChance()
+{
+	userChoice=0
+	while [ $userChoice -eq 0 ]
+	do
+		read -p "			Enter the cell number to make a move :" userChoice
+		if [[ $userChoice -gt 0 && $userChoice -lt 10 ]]
+		then
+			cellOccupied=0
+			for((cell=1;cell<=9;cell++))
+			do
+				if [ $cell -eq $userChoice  ]
+				then
+					if [[ "${gameBoard[cell]}" == "X" || "${gameBoard[cell]}" == "O"  ]]
+					then
+					 ((cellOccupied++))
+					else
+						gameBoard[cell]=$userSign
+					fi
+				fi
+			done
+				if [ $cellOccupied -gt 0 ]
+				then
+					echo "			CELL IS OCCUPIED, ENTER VALID CELL NUMBER"
+					userChoice=0
+				fi
+		else
+			echo "				INVALID INPUT PLEASE RE-ENTER VALID INPUT"
+			userChoice=0
+		fi
+	done
+}
 printf "\n		***** WELCOME TO TIC-TAC-TOE GAME SIMULATOR ***** \n\n\n "
 resetGameBoard
 toss
 signDecision
+display
+userChance
